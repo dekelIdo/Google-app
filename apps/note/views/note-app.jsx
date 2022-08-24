9c971559363c29ca139b31296424537c0979ea7d
eldadYikne <nmknv99@gmail.com>
2022-08-24 16:46:06 +0300

    24.8 16:45

--- apps/note/views/note-app.jsx
+++ apps/note/views/note-app.jsx
@@ -1,22 +1,42 @@
 import { NoteEdit } from "../cmps/note-edit.jsx"
 import { NoteList } from "../cmps/note-list.jsx"
 import { NoteFilter } from "../cmps/note-filter.jsx"
-
+import { noteService } from "../services/note.service.js"
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
 
-export function NoteApp() {
-    return <Router>
-        <section className="note-app">
-            <NoteFilter />
-            <NoteList />
-            
-            <Switch>
+export class NoteApp extends React.Component {
 
-               
-                <Route path="/note/:noteId" component={NoteEdit} />
-               
-            </Switch>
-        </section>
-    </Router>
+
+    state = {
+        notes:[]
+    }
+
+    componentDidMount() {
+        this.loadNotes()
+    }
+    loadNotes = () => {
+
+        noteService.query()
+            .then((notes) => this.setState({ notes }))
+    }
+
+
+    render() {
+        console.log(this.state.notes);
+        if (!this.state.notes) return <span></span>
+        const { notes } = this.state
+        return <Router>
+            <section className="note-app">
+                
+                
+                <NoteList notes={notes} />
+                {/* <NoteFilter /> */}
+
+                <Switch>
+                    <Route path="/note/:noteId" component={NoteEdit} />
+                </Switch>
+            </section>
+        </Router>
+    }
 }
