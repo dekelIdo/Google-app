1cf6fc5d9ab04111900307de60ebc10e6ff0c5ed
dekel ido <dookido@gmail.com>
2022-08-24 13:12:26 +0300

    starter

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -3,11 +3,13 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <h3 className="fa logo"></h3>
         </Link>
         <nav>
             <NavLink exact to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
+            <NavLink  to="/note">Note</NavLink>
+            <NavLink to="/mail">Mail</NavLink>
         </nav>
     </header>
 }
--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,23 @@
+const { Link, withRouter } = ReactRouterDOM
 
 export function Home() {
 
-    return <section className="home">
+    return <section className="home-page">
         <h1>Welcome to home page!</h1>
+
+        <div className="mail-home-img">
+        <Link to="/mail">
+
+    <img src="/assets/img/gmail.jpg" alt=""></img>
+        </Link>
+        </div>
+
+        <div className="note-home-img">
+        <Link to="/note">
+        <img src="/assets/img/note.jpg" alt=""></img>
+        </Link>
+        </div>
+
     </section>
 }
+
