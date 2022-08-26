334ead63031e3e3da6b7dee716d2a4b351fdbe2c
eldadYikne <nmknv99@gmail.com>
2022-08-26 15:56:08 +0300

    15:56

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,18 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <div className="about-us dekel"> 
+        <img src="../img/dekel.jpeg"/>
+        <h2>Dekel ido</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, repudiandae! Provident accusamus sed quidem. Autem fugiat distinctio laboriosam! Ipsam saepe qui exercitationem doloribus facere, aspernatur non soluta placeat vitae labore.</p>
+        </div>
+        <div className="about-us eldad"> 
+
+        <img src="../img/eldad.jpeg"/>
+        <h2>Eldad yikne</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, fugiat tempore ut quos voluptatibus rerum minima, vitae animi nihil aut tenetur. Quos mollitia vero ratione repellendus alias sit qui quo.</p>
+        </div>
+
+
     </section>
 }
