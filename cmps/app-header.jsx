9ae4087df7faf400b44bb3f2c89ff6ecbd43fc83
eldadYikne <nmknv99@gmail.com>
2022-08-28 01:19:19 +0300

    imgae

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -14,14 +14,14 @@ export class AppHeader extends React.Component {
 
         return <header className="app-header">
             <Link to="/">
-                <img className="logo" src="../img/google.png" />
+                <img className="logo" src="./img/google.png" />
             </Link>
 
             <nav className={isOpen ? 'menu-open' : ''}>
-                <NavLink exact to="/"><img src="../img/home.png" onClick={this.toggleMenu}title='Home' /></NavLink>
-                <NavLink to="/about"><img src="../img/about.png" onClick={this.toggleMenu} title='about us'/></NavLink>
-                <NavLink to="/note"><img src="../img/notes.png" onClick={this.toggleMenu}title='notes' /></NavLink>
-                <NavLink to="/mail"><img src="../img/gmailll.png" onClick={this.toggleMenu}title='mail' /></NavLink>
+                <NavLink exact to="/"><img src="./img/home.png" onClick={this.toggleMenu}title='Home' /></NavLink>
+                <NavLink to="/about"><img src="./img/about.png" onClick={this.toggleMenu} title='about us'/></NavLink>
+                <NavLink to="/note"><img src="./img/notes.png" onClick={this.toggleMenu}title='notes' /></NavLink>
+                <NavLink to="/mail"><img src="./img/gmailll.png" onClick={this.toggleMenu}title='mail' /></NavLink>
             </nav>
             <img src="../img/menu.png" className="menu" onClick={this.toggleMenu} />
         </header >
